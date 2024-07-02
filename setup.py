cef0a17f275b108a2936e5d6d2edec6856b9f8aa
Coues10624 <btfalltraining23@gmail.com>
2024-07-02 11:01:25 -0400

    step 2 setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+	name = 'Test',
+	version='0.1',
+	description = 'simple demo package'
+	url = 'https://github.com/Coues10624/M09_practice_repo.git',
+	author = 'Brennan Thompson',
+	author_email = 'btfalltraining2023@gmail.com',
+	license = 'MIT',
+	packages = ['demo']
+)
